70e62407475cc094fa3bc8c9e730ec063aa8881e
tqamarVT <53414514+tqamarVT@users.noreply.github.com>
2019-09-24 20:52:19 -0400

    Add files via upload
    Initial Commit, pushing the project runner class for the project. It is located inside of the "default" package in eclipse.

---
+++ Coursemanager2.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * This is the Project Runner class for this project, it serves as the entry
+ * point to the program. 
+ * 
+ * @author Taimoor Qamar
+ * @version 2019.09.24
+ *
+ */
+public class Coursemanager2 {
+
+    /**
+     * 
+     */
+    public Coursemanager2() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
