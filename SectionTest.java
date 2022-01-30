74850b5fb0f18a8952543195c4d13d676fe8d0a2
tqamarVT <53414514+tqamarVT@users.noreply.github.com>
2022-01-30 18:08:44 -0500

    Update SectionTest.java

--- SectionTest.java
+++ SectionTest.java
@@ -2,15 +2,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 /**
- * // Virginia Tech Honor Code Pledge:
- * //
- * // As a Hokie, I will conduct myself with honor and integrity at all times.
- * // I will not lie, cheat, or steal, nor will I accept the actions of those
- * // who do.
- * //
- * 
- */
-/**
  * Test class for the section class.
  * 
  * @author Taimoor Qamar
