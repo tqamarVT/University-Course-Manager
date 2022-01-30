82a428e2af07c063c92acbb06731fc717ca99b95
tqamarVT <53414514+tqamarVT@users.noreply.github.com>
2022-01-30 18:08:13 -0500

    Update SectionState.java

--- SectionState.java
+++ SectionState.java
@@ -1,15 +1,9 @@
-// Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those who
-// do.
-// -- Peter Dolan (peter8)
 
 /**
  * All of the possible modes for a section
  * 
- * @author Peter Dolan
- * @version 10/6/19
+ * @author Taimoor Qamar (tqamar)
+ * @version 2019.07.15
  *
  */
 public enum SectionState {
