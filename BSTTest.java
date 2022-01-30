5651eb0b8ffc84fbc2f59985c841b9f8cf7cf414
tqamarVT <53414514+tqamarVT@users.noreply.github.com>
2022-01-30 17:41:26 -0500

    Update BSTTest.java

--- BSTTest.java
+++ BSTTest.java
@@ -1,14 +1,5 @@
 import java.util.Iterator;
 
-/**
- * //Virginia Tech Honor Code Pledge:
- * //
- * //As a Hokie, I will conduct myself with honor and integrity at all times.
- * //I will not lie, cheat, or steal, nor will I accept the actions of those
- * //who do.
- * //
- * 
- */
 
 /**
  * Fill in later
